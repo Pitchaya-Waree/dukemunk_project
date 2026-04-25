4afed197f79e1389d0f9d03fd37b910f1cc7b8f7
Pitchaya Wareerattanaroj <runninghaha123@gmail.com>
2026-04-26 04:01:44 +0700

    Add initial page layout with Navbar, Hero, Reservation, and Footer components

---
+++ app/pages/pages.jsx
@@ -0,0 +1,19 @@
+'use client'; // ต้องมีคำนี้เพราะเราใช้ useState ใน Reservation
+import React from 'react';
+import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
+import Story from '@/components/Story';
+import Reservation from '@/components/Reservation';
+import Footer from '@/components/Footer';
+
+export default function table() {
+  return (
+    <main>
+      <Hero />
+      <Navbar />
+      {/* <Story /> */}
+      <Reservation />
+      <Footer />
+    </main>
+  );
+}
